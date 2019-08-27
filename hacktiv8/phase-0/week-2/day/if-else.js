be643d6f018733d20e8186ae6800de625e7476ba
ujangaja <ujangaja@gmail.com>
2019-08-27 21:52:10 +0700

    perbaikan

---
+++ hacktiv8/phase-0/week-2/day/if-else.js
@@ -0,0 +1,15 @@
+var nama = 'Mikael';
+
+var peran = 'Tabib';
+
+if (nama == 'Mikael') {
+    console.log('Halo Ksatria ' + nama + ', Pilih peranmu untuk memulai game!');
+} else if (nama == 'Nina') {
+    console.log('selamat datang di Dunia Proxytia,' + nama + 'Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+} else if (nama == 'Danu') {
+    console.log('selamat datang di Dunia Proxytia,' + nama + ' Halo Tabib Danu ' + nama + ', kamu akan membantu temanmu yang terluka.');
+} else if (nama == 'Zero') {
+    console.log('selamat datang di Dunia Proxytia,' + nama + ' Halo Penyihir Zero ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+} else {
+    console.log('nama harus di isi!');
+}
