05f96af9fe2f7a5be4cf6196d62173481bfbcf88
ujangaja <ujangaja@gmail.com>
2019-08-28 15:22:46 +0700

    update data

--- hacktiv8/phase-0/week-2/day2/if-else.js
+++ hacktiv8/phase-0/week-2/day2/if-else.js
@@ -1,15 +1,15 @@
-var nama = 'Mikael';
+var nama = 'Zero';
 
 var peran = 'Tabib';
 
 if (nama == 'Mikael') {
     console.log('Halo Ksatria ' + nama + ', Pilih peranmu untuk memulai game!');
 } else if (nama == 'Nina') {
-    console.log('selamat datang di Dunia Proxytia,' + nama + 'Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
+    console.log('Selamat datang di Dunia Proxytia,' + nama + '\n' + 'Halo ' + peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!');
 } else if (nama == 'Danu') {
-    console.log('selamat datang di Dunia Proxytia,' + nama + ' Halo Tabib Danu ' + nama + ', kamu akan membantu temanmu yang terluka.');
+    console.log('Selamat datang di Dunia Proxytia,' + nama + '\n' + ' Halo ' + peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka.');
 } else if (nama == 'Zero') {
-    console.log('selamat datang di Dunia Proxytia,' + nama + ' Halo Penyihir Zero ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+    console.log('Selamat datang di Dunia Proxytia,' + nama + '\n' + ' Halo ' + peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
 } else {
     console.log('nama harus di isi!');
 }
