9568db00d8dd7d6e2d2582f8748c817ad28e1df3
ujangaja <ujangaja@gmail.com>
2019-08-29 21:14:29 +0700

    perbaikan pseudocode

--- hacktiv8/phase-0/week-2/day1/day1.js
+++ hacktiv8/phase-0/week-2/day1/day1.js
@@ -1,2 +1,9 @@
-var tampung = 5;
-console.log(tampung); // 5
+var tahun = 2000;
+
+if (tahun % 4 === 0 && tahun % 100 === 0 && tahun % 400 === 0 && tahun % 500 === 0) {
+    console.log("ini Tahun kabisat");
+} else if (tahun % 4 === 0 && tahun % 100 != 0) {
+    console.log("Ini tahun kabisat");
+} else {
+    console.log("Bukan tahun kabisat");
+};
