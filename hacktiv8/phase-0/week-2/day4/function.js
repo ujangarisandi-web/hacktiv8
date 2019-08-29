754e642b84093193c7e5a49bb6b573ffc055c8b9
ujangaja <ujangaja@gmail.com>
2019-08-29 14:14:55 +0700

    menambahkan tugas day4 function

--- hacktiv8/phase-0/week-2/day4/function.js
+++ hacktiv8/phase-0/week-2/day4/function.js
@@ -1,32 +1,30 @@
 // Tugas 1
-// function shoutOut() {
-//     console.log("Halo Function");
-// }
-// shoutOut();
+function shoutOut() {
+    console.log("Halo Function");
+}
+shoutOut();
 
 // Tugas 2
-// function calculateMultiply (){
+function calculateMultiply(num1, num2) {
+    return num1 * num2;
+}
 
-// }
+var num1 = 5;
+var num2 = 6;
+
+var hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian);
 
 
 
 // Tugas 3
 
-function processSentence(fullSentence) {
-    var name = "Agus";
-    var age = 30;
-    var address = "jln. Malioboro, Yogjakarta";
-    var hobby = "gaming";
+function processSentence(name, age, address, hobby) {
+    return 'Nama saya ' + name + ', umur saya ' + age + 'alamat saya ' + address + ' dan hobby saya ' + hobby + '!';
 }
+var name = "Agus";
+var age = 30;
+var address = "jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
 var fullSentence = processSentence(name, age, address, hobby);
 console.log(fullSentence);
-
-// function gabungan() {
-//     var str1 = "Hello ";
-//     var str2 = "Nama Saya";
-//     var str3 = " Andi!";
-//     var res = str1 + str2 + str3;
-//     document.getElementById("demo").innerHTML = res;
-// }
-// </script>
