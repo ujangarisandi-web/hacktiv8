1c433b69917e441a81555f0ff67055fcd8a6addd
ujangaja <ujangaja@gmail.com>
2019-08-29 21:21:17 +0700

    update perubahan week2 day1

--- hacktiv8/phase-0/week-2/day1/day1.js
+++ hacktiv8/phase-0/week-2/day1/day1.js
@@ -1,6 +1,6 @@
 var tahun = 2000;
 
-if (tahun % 4 === 0 && tahun % 100 === 0 && tahun % 400 === 0 && tahun % 500 === 0) {
+if (tahun % 4 === 0 && tahun % 100 === 0 && tahun % 400 === 0) {
     console.log("ini Tahun kabisat");
 } else if (tahun % 4 === 0 && tahun % 100 != 0) {
     console.log("Ini tahun kabisat");
