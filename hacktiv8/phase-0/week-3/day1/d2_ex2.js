620dbc9a3c3b9e22bbdb60488c60b936b576db31
ujangaja <ujangaja@gmail.com>
2019-09-04 07:51:27 +0700

    perbarui d2_ex2.js

--- hacktiv8/phase-0/week-3/day1/d2_ex2.js
+++ hacktiv8/phase-0/week-3/day1/d2_ex2.js
@@ -1,15 +1,13 @@
 /*
     input "hello world!"
     output
- "!dlrow olleh" 
- 
+"!dlrow olleh" 
+
  */
 
 function balikKata(str) {
-    // var balikKata = str;
     var newString = '';
     for (var i = str.length - 1; i--;) {
-        // newString = newString + balikKata[i];
         newString += str[i]
 
     }
