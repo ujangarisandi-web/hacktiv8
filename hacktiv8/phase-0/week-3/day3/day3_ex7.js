9bbbc9897a017e3eff9123ae2e9c77546a8b778f
ujangaja <ujangaja@gmail.com>
2019-09-04 23:07:18 +0700

    day3_ex7.js done!

--- hacktiv8/phase-0/week-3/day3/day3_ex7.js
+++ hacktiv8/phase-0/week-3/day3/day3_ex7.js
@@ -17,3 +17,15 @@ console.log(hitungJumlahKata('I')); // 1
 console.log(hitungJumlahKata('I believe I can code')); // 5
 
  */
+
+function hitungJumlahKata(kalimat) {
+
+    var array = kalimat.split(" ");
+    return array.length;
+}
+
+console.log(hitungJumlahKata('I have a dream')); // 4
+console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+console.log(hitungJumlahKata('A song to sing')); // 4
+console.log(hitungJumlahKata('I')); // 1
+console.log(hitungJumlahKata('I believe I can code')); // 5
