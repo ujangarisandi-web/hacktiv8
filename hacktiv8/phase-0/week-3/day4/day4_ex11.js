101875112cd34f9e36339411b532058c701be549
ujangaja <ujangaja@gmail.com>
2019-09-05 17:40:28 +0700

    uodate data day_ex11.js

--- hacktiv8/phase-0/week-3/day4/day4_ex11.js
+++ hacktiv8/phase-0/week-3/day4/day4_ex11.js
@@ -22,27 +22,30 @@ console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
 
 
 function tentukanDeretAritmatika(arr) {
-    var result;
-    var angkaPertma;
-    var angkaKedua;
 
-    for (var i = 0; i < arr.length - 2; i++) {
-        angkaPertma = arr[i + 1] - arr[i];
-        // angkaKedua = arr[i + 2] - arr[i + 1];
+  var deretPertambahan = [];
+
+  // code untuk mengecek pertambahan tiap deret dan memasukkannya ke array deretPertambahan
+  for (var i = 0; i < arr.length - 1; i++) {
+    var pertambahan = arr[i + 1] - arr[i];
+
+    deretPertambahan.push(pertambahan);
+  }
+  // return pertambahan; //cek hasil pertambahan
+  // return deretPertambahan; //cek hasil pertambahan dan ubah kedalam bentuk array
 
 
-        // angkaPertma === angkaKedua ?
-        //     result = true :
-        //     result = false;
+  // code untuk mengecek apakah elemen pada array deretPertambahan memiliki elemen yang sama semua
+  for (var j = 0; j <= deretPertambahan.length - 1; j++) {
+    if (deretPertambahan[j] !== deretPertambahan[0]) {
+      return false;
     }
-    // return result;
-    return angkaPertma;
-    // return ankaKedua;
-
+  }
+  return true
 }
 // TEST CASES
 console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
 console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
-// console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
-// console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
-// console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
