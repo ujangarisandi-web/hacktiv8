d3fc81a92c23ddca73f98af0c512e10ec0046473
ujangaja <ujangaja@gmail.com>
2019-09-05 07:45:30 +0700

    cara lebih mudah untuk d2_ex3.js

--- hacktiv8/phase-0/week-3/day1/d2_ex3.js
+++ hacktiv8/phase-0/week-3/day1/d2_ex3.js
@@ -9,6 +9,10 @@ var input = [
 function dataHandling(input) {
     for (var i = 0; i < input.length; i++) {
         console.log('Nomor ID : ' + input[i][0]);
+        console.log('Nama Lengkap : ' + input[i][1]);
+        console.log('Ttl : ' + input[i][2] + ' ' + input[i][3]);
+        console.log('Hobi : ' + input[i][4]);
+        console.log('\n');
     }
 }
 dataHandling(input);
