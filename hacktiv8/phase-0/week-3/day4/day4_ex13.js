e4a489d212cc605fa184f3bc19ff593d08d28973
ujangaja <ujangaja@gmail.com>
2019-09-06 18:44:37 +0700

    up day4_ex13.js

---
+++ hacktiv8/phase-0/week-3/day4/day4_ex13.js
@@ -0,0 +1,4 @@
+/* Logic Challenge - Target Terdekat
+Problem
+
+Diberikan sebuah function targetTerdekat(arr) yang menerima satu parameter berupa array yang terdiri dari karakter. Function akan me-return jarak spasi antar karakter 'o' dengan karakter 'x' yang terdekat. Contoh, jika arr adalah ['x', ' ', 'o', ' ', ' ', 'x'], maka jarak terdekat dari 'o' ke 'x' adalah 2. Jika tidak ditemukan 'x' sama sekali, function akan me-return nilai 0. */
