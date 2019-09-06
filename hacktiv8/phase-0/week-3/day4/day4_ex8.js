6d7b0012172489e06f25875749ddb6c98bd3a8b6
ujangaja <ujangaja@gmail.com>
2019-09-06 20:42:41 +0700

    update data

--- hacktiv8/phase-0/week-3/day4/day4_ex8.js
+++ hacktiv8/phase-0/week-3/day4/day4_ex8.js
@@ -1,29 +1,19 @@
-/* Logic Challenge - Mencari Mean
+/* Logic Challenge - Pasangan Angka Terbesar
 Problem
 
-Diberikan sebuah function cariMean(arr) yang menerima sebuah array angka. Function akan me-return mean dari array atau deret angka tersebut. Mean adalah angka rata-rata dari deret bilangan tersebut. Contoh, mean atau rata-rata dari [1, 2, 3, 4, 5] adalah 3. Kita perlu kemudian melakukan pembulatan angka dari hasil mean yang didapatkan (pembulatan ke atas).
+Diberikan sebuah function pasanganTerbesar(angka) yang menerima 1 parameter berupa angka. Functiona akan menentukan pasangan dua digit angka mana yang paling besar dan me-return angka tersebut. Contoh, jika angka adalah 183928, maka function akan me-return 92, pasangan dua digit angka yang paling besar diantara yang lainnya.
 
-function cariMean(arr) {
+CODE :
+
+function pasanganTerbesar(num) {
   // you can only write your code here!
 }
 
 // TEST CASES
-console.log(cariMean([1, 2, 3, 4, 5])); // 3
-console.log(cariMean([3, 5, 7, 5, 3])); // 5
-console.log(cariMean([6, 5, 4, 7, 3])); // 5
-console.log(cariMean([1, 3, 3])); // 2
-console.log(cariMean([7, 7, 7, 7, 7])); // 7
-*/
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
 
-function cariMean(arr) {
-    var hasilJumlah = 0;
-    for (var i = 0; i < arr.length; i++) {
-        hasilJumlah += arr[i];
-    }
-    return Math.round(hasilJumlah / arr.length);
-}
-console.log(cariMean([1, 2, 3, 4, 5])); //3
-console.log(cariMean([3, 5, 7, 5, 3])); // 5
-console.log(cariMean([6, 5, 4, 7, 3])); // 5
-console.log(cariMean([1, 3, 3])); // 2
-console.log(cariMean([7, 7, 7, 7, 7])); // 7
+*/
