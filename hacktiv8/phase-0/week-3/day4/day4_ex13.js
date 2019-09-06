df5b93e0ae35268bbfae1aaf605711af9770b972
ujangaja <ujangaja@gmail.com>
2019-09-06 20:31:47 +0700

    day4_ex14.js done

--- hacktiv8/phase-0/week-3/day4/day4_ex13.js
+++ hacktiv8/phase-0/week-3/day4/day4_ex13.js
@@ -1,4 +1,63 @@
 /* Logic Challenge - Target Terdekat
 Problem
 
-Diberikan sebuah function targetTerdekat(arr) yang menerima satu parameter berupa array yang terdiri dari karakter. Function akan me-return jarak spasi antar karakter 'o' dengan karakter 'x' yang terdekat. Contoh, jika arr adalah ['x', ' ', 'o', ' ', ' ', 'x'], maka jarak terdekat dari 'o' ke 'x' adalah 2. Jika tidak ditemukan 'x' sama sekali, function akan me-return nilai 0. */
+Diberikan sebuah function targetTerdekat(arr) yang menerima satu parameter berupa array yang terdiri dari karakter. Function akan me-return jarak spasi antar karakter 'o' dengan karakter 'x' yang terdekat. Contoh, jika arr adalah ['x', ' ', 'o', ' ', ' ', 'x'], maka jarak terdekat dari 'o' ke 'x' adalah 2. Jika tidak ditemukan 'x' sama sekali, function akan me-return nilai 0. 
+
+
+CODE
+
+function targetTerdekat(arr) {
+  // you can only write your code here!
+}
+
+// TEST CASES
+console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
+
+*/
+
+function targetTerdekat(arr) {
+    var indexO = [];
+    var indexX = [];
+    var arrBanding = [];
+
+
+    // code untuk mencari index dari x dan o
+    for (var i = 0; i <= arr.length - 1; i++) {
+        if (arr[i] == "o") {
+            indexO.push(i)
+        } else if (arr[i] == "x") {
+            indexX.push(i)
+        }
+    }
+
+    // apabila tdk ada x sama sekali, return 0
+    if (indexX.length == 0) {
+        return 0;
+    }
+
+    // menghitung perbandingan jarak antar x, kemudian di push ke array arrBanding
+    var jarak;
+    for (var j = 0; j <= indexX.length - 1; j++) {
+        jarak = Math.abs(indexO[0] - indexX[j])
+        arrBanding.push(jarak)
+    }
+
+    arrBanding.sort(function (a, b) {
+        return a - b
+    })
+
+    return arrBanding[0];
+
+    // return indexX;
+
+}
+
+console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
---
+++ hacktiv8/phase-0/week-3/day4/day4_ex15.js
@@ -0,0 +1,52 @@
+/* Logic Challenge - Mengelompokkan Hewan
+Problem
+
+Diberikan sebuah function groupAnimals(arr) yang menerima satu parameter berupa array yang berisi string. Function akan me-return array multidimensi dimana array tersebut berisikan kategori/kelompok array yang dikumpulkan sesuai dengan abjad depannya. Abjad harus berurut dari a-z (jika ada). Untuk kasus ini, anggap saja semua text lowercase.
+
+Contoh jika animals adalah ["ayam", "kucing", "bebek", "bangau", "zebra"]
+
+maka akan menampilkan output: [ [ "ayam" ], [ "bebek", "bangau" ], [ "kucing ], [ "zebra" ] ]
+
+Urutan hewan dalam array setiap pengelompokan huruf tidak penting.
+
+Code:
+
+function groupAnimals(animals) {
+  // you can only write your code here!
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
+
+*/
+
+
+function groupAnimals(arr) {
+    var kamus = 'abcdefghijklmnopqrstuvwxyz';
+    var newArr = [];
+
+    // code pengurutann abjad
+    for (var i = 0; i <= kamus.length - 1; i++) {
+        var gather = [];
+
+        // code pengumpulan hewan berdasarkan abjad alpha
+        for (var j = 0; j <= arr.length - 1; j++) {
+            if (arr[j][0] === kamus[i]) {
+                gather.push(arr[j])
+            }
+        }
+        if (gather.length > 0) {
+            newArr.push(gather);
+        }
+
+    }
+    return newArr;
+}
+
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
