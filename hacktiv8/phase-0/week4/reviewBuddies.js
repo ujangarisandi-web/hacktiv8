e07aacf141cf4e6a4e5e384bb7971ded10c1f239
ujangaja <ujangaja@gmail.com>
2019-09-11 22:36:40 +0700

    tambah data review buddies.js

---
+++ hacktiv8/phase-0/week4/reviewBuddies.js
@@ -0,0 +1,38 @@
+/* 
+buatlah sebuah fungsi perulangan, untuk mengitung jumlah total index dari kalimat/nama
+contoh:
+n+o+v+i+a+n+t+i
+1+2+3+4+5+6+7+8 = 36
+*/
+console.log('=======satu=========');
+nilai = 0
+
+function jumlahIndexNama(nama) {
+    for (var i = 0; i < nama.length; i++) {
+        nilai += i + 1
+    }
+    return nilai
+}
+console.log(jumlahIndexNama('novianti'));
+console.log(jumlahIndexNama('ujang arisandi'));
+console.log('=======dua=========');
+
+str = ''
+nilai2 = 0
+abjad = 'abcdefghijklmnopqrstuvwxyz '
+
+function jumlahIndexNama2(nama) {
+    for (var i = 0; i < nama.length; i++) {
+        for (var j = 0; j < abjad.length; j++) {
+            if (nama[i] === abjad[j]) {
+                // nilai2 += j + 1
+                nilai2 += j + 1
+
+
+            }
+        }
+    }
+    return nilai2
+}
+console.log(jumlahIndexNama2('ujang arisandi'));
+// jumlahIndexNama2('ujang arisandi')
