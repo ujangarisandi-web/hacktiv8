6dca3b9230b0609ac84b5e1aeea867eba869ba09
ujangaja <ujangaja@gmail.com>
2019-09-11 23:14:43 +0700

    belum nemu

--- hacktiv8/phase-0/week4/day4_ex2.js
+++ hacktiv8/phase-0/week4/day4_ex2.js
@@ -17,3 +17,16 @@ console.log(fpb(24, 36)); // 12
 console.log(fpb(17, 23)); // 1
 
 */
+var angka1;
+
+function fpb(angka1, angka2) {
+    var nilai = 0
+    for (var i = 0; i < angka1.lengt; i++) {
+        if (angka1)
+    }
+}
+console.log(fpb(12, 16)); // 4
+console.log(fpb(50, 40)); // 10
+console.log(fpb(22, 99)); // 11
+console.log(fpb(24, 36)); // 12
+console.log(fpb(17, 23)); // 1
