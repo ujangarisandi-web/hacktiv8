3da64aeb9f5e8d288450b6bbddcc748a54160c3e
ujangaja <ujangaja@gmail.com>
2019-09-12 23:01:59 +0700

    kalimat tertukar done

--- hacktiv8/phase-0/week4/day3/day3_ex8.js
+++ hacktiv8/phase-0/week4/day3/day3_ex8.js
@@ -16,3 +16,22 @@ console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
 console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
 
 */
+
+function tukarBesarKecil(kalimat) {
+    var tertukar = ''
+    for (var i = 0; i < kalimat.length; i++) {
+        if (kalimat[i] === kalimat[i].toUpperCase()) {
+            tertukar += kalimat[i].toLowerCase()
+        } else if (kalimat[i] === kalimat[i].toLowerCase()) {
+            tertukar += kalimat[i].toUpperCase()
+        } else {
+            tertukar += kalimat[i]
+        }
+    }
+    return tertukar
+}
+console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
