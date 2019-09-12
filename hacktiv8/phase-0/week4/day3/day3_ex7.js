41de1c3d9b423fe98ce7ed6dbb5552a90b1883c7
ujangaja <ujangaja@gmail.com>
2019-09-12 22:40:16 +0700

    urutkanAbjad done

--- hacktiv8/phase-0/week4/day3/day3_ex7.js
+++ hacktiv8/phase-0/week4/day3/day3_ex7.js
@@ -17,3 +17,12 @@ console.log(urutkanAbjad('software')); // 'aeforstw'
 console.log(urutkanAbjad('aegis')); // 'aegis'
 
 */
+
+function urutkanAbjad(str) {
+    return str.split('').sort().join('')
+}
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
