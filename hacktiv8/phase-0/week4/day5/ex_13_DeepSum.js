896eca5c80a7d1a32b081818596f898bd2bc8446
ujangaja <ujangaja@gmail.com>
2019-09-13 21:49:54 +0700

    ex_13_DeepSum done!

---
+++ hacktiv8/phase-0/week4/day5/ex_13_DeepSum.js
@@ -0,0 +1,126 @@
+/* Logic Challenge: Deep Sum
+
+Implementasikan function deepSum untuk mendapatkan jumlah pertambahan dari angka-angka yang terdapat di dalam array
+
+Contoh array:
+
+[
+  [
+    [1, 2, 3],
+    [1, 2, 3]
+  ],
+  [
+    [1, 2, 3],
+    [1, 2],
+    [1]
+  ]
+]
+Maka akan menghasilkan output: 22
+
+Level kedalaman array akan di-asumsikan selalu 3, [ [ [ ] ] ], jika yang diberikan adalah array kosong maka kembalikan nilai 'No number'
+
+
+function deepSum (arr) {
+  // Code disini
+}
+
+//TEST CASE
+console.log(deepSum([
+  [
+    [4, 5, 6],
+    [9, 1, 2, 10],
+    [9, 4, 3]
+  ],
+  [
+    [4, 14, 31],
+    [9, 10, 18, 12, 20],
+    [1, 4, 90]
+  ],
+  [
+    [2, 5, 10],
+    [3, 4, 5],
+    [2, 4, 5, 10]
+  ]
+])); // 316
+
+console.log(deepSum([
+  [
+    [20, 10],
+    [15],
+    [1, 1]
+  ],
+  [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    [2],
+    [9, 11]
+  ],
+  [
+    [3, 5, 1],
+    [1, 5, 3],
+    [1]
+  ],
+  [
+    [2]
+  ]
+])); // 156
+
+console.log(deepSum([])); // No number
+*/
+
+
+function deepSum(arr) {
+    var sum = 0
+    for (var i = 0; i < arr.length; i++) { // array pertama
+        for (var j = 0; j < arr[i].length; j++) { //array kedua
+            for (var k = 0; k < arr[i][j].length; k++) { //Array ke tiga 
+                sum += arr[i][j][k]
+            }
+        }
+    }
+    if (arr.length === 0) { // untuk data array yang kosong
+        return 'No number'
+    }
+    return sum
+}
+
+//TEST CASE
+console.log(deepSum([
+    [
+        [4, 5, 6],
+        [9, 1, 2, 10],
+        [9, 4, 3]
+    ],
+    [
+        [4, 14, 31],
+        [9, 10, 18, 12, 20],
+        [1, 4, 90]
+    ],
+    [
+        [2, 5, 10],
+        [3, 4, 5],
+        [2, 4, 5, 10]
+    ]
+])); // 316
+
+console.log(deepSum([
+    [
+        [20, 10],
+        [15],
+        [1, 1]
+    ],
+    [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        [2],
+        [9, 11]
+    ],
+    [
+        [3, 5, 1],
+        [1, 5, 3],
+        [1]
+    ],
+    [
+        [2]
+    ]
+])); // 156
+
+console.log(deepSum([])); // No number
