a8833049dfcef29c1f58c6635c9932530fe6cf3d
ujangaja <ujangaja@gmail.com>
2019-09-14 13:32:54 +0700

    latihan array

---
+++ hacktiv8/phase-0/week4/day6Latihan/arr.js
@@ -0,0 +1,23 @@
+/* 
+mencari kelipatan dua dari array
+*/
+
+function kelipatanDua() {
+    var arr = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8],
+        [9, 10, 11, 12]
+    ]
+    var tamp = []
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] % 2 === 0) {
+                tamp.push(arr[i][j])
+            }
+        }
+
+    }
+    return tamp
+}
+console.log(kelipatanDua());
