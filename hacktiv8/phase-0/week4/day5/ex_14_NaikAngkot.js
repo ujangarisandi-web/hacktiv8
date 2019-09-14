4e8b52ef827584cf7467ac5c5e2cd6407a11c183
ujangaja <ujangaja@gmail.com>
2019-09-14 21:26:28 +0700

    update perbaikan  Naikangkot

--- hacktiv8/phase-0/week4/day5/ex_14_NaikAngkot.js
+++ hacktiv8/phase-0/week4/day5/ex_14_NaikAngkot.js
@@ -25,16 +25,18 @@ console.log(naikAngkot([])); //[]
 
 function naikAngkot(arrPenumpang) {
     rute = ['A', 'B', 'C', 'D', 'E', 'F'];
-    var laporan = []
+    var info = []
     for (var i = 0; i < arrPenumpang.length; i++) {
-        laporan.push({
-            penumpang: arrPenumpang[i][0],
-            naikDari: arrPenumpang[i][1],
-            tujuan: arrPenumpang[i][2],
-            bayar: 2000 * (rute.indexOf(arrPenumpang[i][2]) - rute.indexOf(arrPenumpang[i][1]))
-        })
+        info.push({
+                penumpang: arrPenumpang[i][0],
+                naikDari: arrPenumpang[i][1],
+                tujuan: arrPenumpang[i][2],
+                bayar: 2000 * (rute.indexOf(arrPenumpang[i][2]) - rute.indexOf(arrPenumpang[i][1]))
+            }
+
+        )
     }
-    return laporan
+    return info
 }
 
 //TEST CASE
