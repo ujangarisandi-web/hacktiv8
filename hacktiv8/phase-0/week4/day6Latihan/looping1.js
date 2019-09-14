b4b05796f77215650817d87dbeff38b45885e36d
ujangaja <ujangaja@gmail.com>
2019-09-14 16:25:17 +0700

    latihan palindrome

--- hacktiv8/phase-0/week4/day6Latihan/looping1.js
+++ hacktiv8/phase-0/week4/day6Latihan/looping1.js
@@ -1,1 +1,21 @@
 console.log('======Paindrome======');
+
+function palindrome(kata) {
+    var lawanKata = ''
+    for (var i = kata.length - 1; i >= 0; i--) {
+        lawanKata += kata[i]
+
+    }
+    if (kata === lawanKata) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
