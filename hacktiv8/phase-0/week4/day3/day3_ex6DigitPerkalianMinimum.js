6a63676ea5377ce0ceaf00dfff9dd2befef3d3d5
ujangaja <ujangaja@gmail.com>
2019-09-16 22:02:00 +0700

    digit perkalian minimum

---
+++ hacktiv8/phase-0/week4/day3/day3_ex6DigitPerkalianMinimum.js
@@ -0,0 +1,4 @@
+/* Logic Challenge - Digit Perkalian Minimum
+Problem
+
+Diberikan sebuah function digitPerkalianMinimum(angka) yang menerima satu parameter angka. Function akan mengembalikan jumlah digit minimal dari angka yang merupakan faktor angka parameter tersebut, Contoh, jika angka adalah 24, maka faktornya adalah 1 * 24, 2 * 12, 3 * 8, dan 4 * 6. Dari antara faktor tersebut, yang digit nya paling sedikit adalah 3 * 8 atau 4 * 6, sehingga function akan me-return 2. */
