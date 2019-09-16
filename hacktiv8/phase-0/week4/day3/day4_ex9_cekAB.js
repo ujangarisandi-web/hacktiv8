ca6a133e1e76a5f6db297832dcb230686e5eaa18
ujangaja <ujangaja@gmail.com>
2019-09-16 21:54:24 +0700

    Chek AB done!

--- hacktiv8/phase-0/week4/day3/day4_ex9_cekAB.js
+++ hacktiv8/phase-0/week4/day3/day4_ex9_cekAB.js
@@ -18,3 +18,24 @@ console.log(checkAB('barbarian')); // true
 console.log(checkAB('bacon and meat')); // false
 
 */
+
+function checkAB(num) {
+    // you can only write your code here!
+    var strA = ''
+    var strB = ''
+    for (var i = 0; i < num.length; i++) {
+        if (num[i] === 'a') {
+            strA = i
+        } else if (num[i] === 'b') {
+            strB = i
+        }
+    }
+    return strA - strB === 4 || strB - strA === 4
+}
+
+// TEST CASES
+console.log(checkAB('lane borrowed')); // true
+console.log(checkAB('i am sick')); // false
+console.log(checkAB('you are boring')); // true
+console.log(checkAB('barbarian')); // true
+console.log(checkAB('bacon and meat')); // false
