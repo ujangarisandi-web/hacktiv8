0cf86dd0fe9abd3e08b2dffb8bd06ada3523bc05
ujangaja <ujangaja@gmail.com>
2019-09-16 22:46:53 +0700

    perkalian minimum, masih ada yang undifined

--- hacktiv8/phase-0/week4/day3/day3_ex6DigitPerkalianMinimum.js
+++ hacktiv8/phase-0/week4/day3/day3_ex6DigitPerkalianMinimum.js
@@ -15,3 +15,30 @@ console.log(digitPerkalianMinimum(20)); // 2
 console.log(digitPerkalianMinimum(179)); // 4
 console.log(digitPerkalianMinimum(1)); // 2
 */
+
+function digitPerkalianMinimum(angka) {
+    // you can only write your code here!
+    var tampung = []
+    for (var i = 0; i <= angka; i++) {
+        for (var j = 0; j <= angka; j++) {
+            if (i * j === angka) {
+                var join = i.toString() + j.toString()
+                tampung.push(join)
+            }
+        }
+    }
+    var hasil = tampung[0].length
+
+    for (var k = 0; k < tampung.length; k++) {
+        if (tampung[k].length < hasil) {
+            hasil = tampung[k.length]
+        }
+    }
+    return hasil
+}
+// TEST CASES
+console.log(digitPerkalianMinimum(24)); // 2
+console.log(digitPerkalianMinimum(90)); // 3
+console.log(digitPerkalianMinimum(20)); // 2
+console.log(digitPerkalianMinimum(179)); // 4
+console.log(digitPerkalianMinimum(1)); // 2
