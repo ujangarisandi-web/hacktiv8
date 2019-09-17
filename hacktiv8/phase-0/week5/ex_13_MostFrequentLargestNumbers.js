1e5b9450dac54310febf6d11dcdb33997e2b013f
ujangaja <ujangaja@gmail.com>
2019-09-18 06:22:59 +0700

    PasswordGenerator ex13

--- hacktiv8/phase-0/week5/ex_13_MostFrequentLargestNumbers.js
+++ hacktiv8/phase-0/week5/ex_13_MostFrequentLargestNumbers.js
@@ -7,7 +7,7 @@ Dengan HANYA mengubah code di dalam 2 function yang diberikan (sorting dan getTo
 
  CODE :
   
-unction sorting(arrNumber) {
+function sorting(arrNumber) {
   // code di sini
 }
 
@@ -34,3 +34,35 @@ console.log(mostFrequentLargestNumbers([]));
 //''
 
 */
+
+function sorting(arrNumber) {
+    // code di sini
+    return arrNumber.sort(function (a, b) {
+        a - b
+    })
+}
+
+function getTotal(arrNumber) {
+    // code di sini
+    if (arrNumber.length === 0) {
+        return ''
+    }
+}
+
+function mostFrequentLargestNumbers(arrNumber) {
+    var listSort = sorting(arrNumber);
+    var countHighest = getTotal(listSort);
+    return countHighest;
+}
+
+console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+
+console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+//'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+
+console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+
+console.log(mostFrequentLargestNumbers([]));
+//''
