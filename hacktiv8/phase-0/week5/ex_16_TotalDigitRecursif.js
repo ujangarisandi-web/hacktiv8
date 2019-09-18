4f3a7587ce1ab80b97592f98a0451ebfb0b67093
ujangaja <ujangaja@gmail.com>
2019-09-18 08:33:03 +0700

    ex_17_KaliTerus.js

--- hacktiv8/phase-0/week5/ex_16_TotalDigitRecursif.js
+++ hacktiv8/phase-0/week5/ex_16_TotalDigitRecursif.js
@@ -21,3 +21,27 @@ console.log(totalDigitRekursif(21)); // 3
 console.log(totalDigitRekursif(11111)); // 5
 
 */
+
+function totalDigitRekursif(angka) {
+    // you can only write your code here!
+    // ubah angka dealam string
+    var toStr = angka.toString()
+    // var total = 0
+    // total += parseInt(toStr[0])
+    // return (toStr.slice(1))
+    return toStr
+    // var total = 0
+    // if (toStr.length === 0) {
+    //     return 0
+    // } else {
+    //     total += parseInt(toStr[0])
+    //     return total + totalDigitRekursif(toStr.slice(1))
+    // }
+}
+
+// TEST CASES
+console.log(totalDigitRekursif(512)); // 8
+console.log(totalDigitRekursif(1542)); // 12
+console.log(totalDigitRekursif(5)); // 5
+console.log(totalDigitRekursif(21)); // 3
+console.log(totalDigitRekursif(11111)); // 5
