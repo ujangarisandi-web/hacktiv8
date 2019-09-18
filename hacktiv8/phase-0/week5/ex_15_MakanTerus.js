b0adf48f868cc9408c01e83f81101e64e97b06ba
ujangaja <ujangaja@gmail.com>
2019-09-18 07:41:28 +0700

    ex_16_TotaldigitRecursif.js

--- hacktiv8/phase-0/week5/ex_15_MakanTerus.js
+++ hacktiv8/phase-0/week5/ex_15_MakanTerus.js
@@ -27,3 +27,16 @@ console.log(makanTerusRekursif(10)); // 1
 console.log(makanTerusRekursif(0)); // 0
 
 */
+
+function makanTerusRekursif(waktu) {
+    // you can only write your code here!
+    var hasil = 0
+
+}
+
+// TEST CASES
+console.log(makanTerusRekursif(66)); // 5
+console.log(makanTerusRekursif(100)); // 7
+console.log(makanTerusRekursif(90)); // 6
+console.log(makanTerusRekursif(10)); // 1
+console.log(makanTerusRekursif(0)); // 0
