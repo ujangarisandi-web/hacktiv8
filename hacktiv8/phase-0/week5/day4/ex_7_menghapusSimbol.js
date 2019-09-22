bd7644b6ebb7524d05d25fd3aacb5f05877818cd
ujangaja <ujangaja@gmail.com>
2019-09-22 18:52:36 +0700

    hapus symbol dengan regex done!

--- hacktiv8/phase-0/week5/day4/ex_7_menghapusSimbol.js
+++ hacktiv8/phase-0/week5/day4/ex_7_menghapusSimbol.js
@@ -17,3 +17,19 @@ console.log(hapusSimbol('coding')); // coding
 console.log(hapusSimbol('1+3-5*2=100')); // 1352100
 
 */
+function hapusSimbol(str) {
+    // you can only write your code here!
+    var tampStr = ''
+    var noSymbol = str.match(/[A-z]|[0-9]/g)
+    for (var i = 0; i < noSymbol.length; i++) {
+        tampStr += noSymbol[i]
+    }
+    return tampStr
+}
+
+// TEST CASES
+console.log(hapusSimbol('test%$4aa')); // test4aa
+console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+console.log(hapusSimbol('ma@#k!an~')); // makan
+console.log(hapusSimbol('coding')); // coding
+console.log(hapusSimbol('1+3-5*2=100')); // 1352100
