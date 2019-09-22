55a11d9df03ada4e5c0177320a2a62d772039a0e
ujangaja <ujangaja@gmail.com>
2019-09-22 18:54:47 +0700

    ex_8_cariPelaku.js

---
+++ hacktiv8/phase-0/week5/day4/ex_8_cariPelaku.js
@@ -0,0 +1,5 @@
+/* Logic Challenge - Cari Pelaku (Regex)
+Problem
+
+Diberikan sebuah function cariPelaku(kalimat) yang menerima satu parameter berupa string. Function akan me-return jumlah berapa kali ditemukan kata "abc" secara berturut-turut di dalam kalimat tersebut. Gunakan regex untuk melatih kemampuan regex.
+ */
