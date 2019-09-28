55bd40c4722612e6c15f5fde514e0bd50aa89fb1
ujangaja <ujangaja@gmail.com>
2019-09-28 16:18:01 +0700

    update data day2

--- hacktiv8/phase-0/week-2/day1/day1.js
+++ hacktiv8/phase-0/week-2/day1/day1.js
@@ -1,3 +1,16 @@
+/* 
+2. Tahun Kabisat
+
+Apa yang kamu ketahui tentang tanggal 29 Februari? Kamu pasti tahu jika suatu tahun memiliki tanggal 29 Februari berarti tahun tersebut merupakan tahun kabisat.
+
+Dalam kalender Gregorian, tahun kabisat memiliki beberapa kriteria yaitu antara lain:
+
+    Jika tahun habis di bagi 4 dan tidak habis di bagi 100, atau
+    Jika tahun habis di bagi 4, habis di bagi 100 dan habis di bagi 400
+
+Buatlah algoritma & pseudocode untuk menentukan apakah suatu tahun merupakan tahun kabisat atau bukan! */
+
+
 var tahun = 2000;
 
 if (tahun % 4 === 0 && tahun % 100 === 0 && tahun % 400 === 0) {
--- hacktiv8/phase-0/week5/ex_13_MostFrequentLargestNumbers.js
+++ hacktiv8/phase-0/week5/ex_13_MostFrequentLargestNumbers.js
@@ -36,23 +36,23 @@ console.log(mostFrequentLargestNumbers([]));
 */
 
 function sorting(arrNumber) {
-    // code di sini
-    return arrNumber.sort(function (a, b) {
-        a - b
-    })
+  // code di sini
+  return arrNumber.sort(function (a, b) {
+    a - b
+  })
 }
 
 function getTotal(arrNumber) {
-    // code di sini
-    if (arrNumber.length === 0) {
-        return ''
-    }
+  // code di sini
+  if (arrNumber.length === 0) {
+    return ''
+  }
 }
 
 function mostFrequentLargestNumbers(arrNumber) {
-    var listSort = sorting(arrNumber);
-    var countHighest = getTotal(listSort);
-    return countHighest;
+  var listSort = sorting(arrNumber);
+  var countHighest = getTotal(listSort);
+  return countHighest;
 }
 
 console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
