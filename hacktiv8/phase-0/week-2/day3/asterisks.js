0decae6d5feb437e393773805588bb70ee1c1987
ujangaja <ujangaja@gmail.com>
2019-09-28 16:54:31 +0700

    update asterik

--- hacktiv8/phase-0/week-2/day3/asterisks.js
+++ hacktiv8/phase-0/week-2/day3/asterisks.js
@@ -1,3 +1,33 @@
+/* 
+Looping
+
+Soal looping - asterisk ini memiliki 3 nomor.
+1. Menyusun Barisan Bintang
+Problem
+
+Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript untuk menampilkan di console barisan asterisks (bintang). Setiap baris memiliki 1 simbol '*'.
+Skeleton Code:
+
+var rows1; // input the number of rows
+
+// do loops to display asterisks in the console.
+
+Output
+
+jika rows1 = 5
+
+*
+*
+*
+*
+*
+
+
+
+
+*/
+
+
 // #Menyusun Barisan Bintang
 
 var row1 = '';
@@ -6,6 +36,30 @@ for (var i = 0; i < 5; i++) {
     row1 += '\n';
 }
 console.log(row1);
+// ===================================================
+/* 
+
+2. Menyusun Barisan Bintang Dengan Nested Looping
+Problem
+
+Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript untuk menampilkan di console barisan asterisks (bintang). Setiap baris memiliki jumlah simbol '*' sesuai dengan jumlah baris. Manfaatkan nested loop atau loop di dalam loop untuk menyelesaikan kasus ini.
+Skeleton Code:
+var rows2; // input the number of rows
+
+// do loops to display asterisks in the console.
+
+Output
+
+jika rows2 = 5
+
+*****
+*****
+*****
+*****
+*****
+
+
+*/
 
 // #2. Menyusun Barisan Bintang Dengan Nested Looping
 var row2 = '';
@@ -17,6 +71,30 @@ for (var i = 0; i < 5; i++) {
     row2 += '\n';
 }
 console.log(row2);
+
+//==========================================================
+
+/* 
+3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+Problem
+
+Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript untuk menampilkan di console barisan asterisks (bintang) dalam bentuk tangga. Setiap baris memiliki jumlah simbol '*' sesuai dengan nomor baris. Baris pertama, hanya ada satu bintang. Baris kedua, dua bintang, baris ketiga tiga bintang, dan seterusnya. Manfaatkan nested loop atau loop di dalam loop untuk menyelesaikan kasus ini.
+Skeleton Code:
+var rows3; // input the number of rows
+
+// do loops to display asterisks in the console.
+
+Output
+
+jika rows3 = 5
+
+*
+**
+***
+****
+*****
+
+*/
 
 // #3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
 
