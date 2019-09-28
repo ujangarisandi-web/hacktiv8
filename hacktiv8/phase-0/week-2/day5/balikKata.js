1a631b0cbeb5b89b56587fa3bfbbccdb43b304ba
ujangaja <ujangaja@gmail.com>
2019-09-28 17:02:34 +0700

    update balik kata

--- hacktiv8/phase-0/week-2/day5/balikKata.js
+++ hacktiv8/phase-0/week-2/day5/balikKata.js
@@ -1,3 +1,11 @@
+/* 
+Logic Challenge - Balik Kata
+Problem
+
+Diberikan sebuah function balikKata(kata) yang menerima satu parameter berupa string. Function akan me-return kata yang dibalik. Contoh, jika kata adalah "John Doe", function akan me-return "eoD nhoJ".
+
+*/
+
 function balikKata(kata) {
     var balik = '';
     for (var i = kata.length - 1; i >= 0; i--) {
