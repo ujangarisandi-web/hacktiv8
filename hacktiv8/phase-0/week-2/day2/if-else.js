db3faf07a09614b3ba0eec1fdceece4da356d0e8
ujangaja <ujangaja@gmail.com>
2019-09-28 16:39:22 +0700

    update data week2 day3

--- hacktiv8/phase-0/week-2/day2/if-else.js
+++ hacktiv8/phase-0/week-2/day2/if-else.js
@@ -1,36 +1,70 @@
-var loop = 1;
-while (loop < 5) {
-    if (loop == 1) {
-        var nama = 'Mikael'
-        var peran = ''
-        console.log('//Output untuk Input ' + loop)
-    } else if (loop == 2) {
-        var nama = 'Nina'
-        var peran = 'ksatria'
-        console.log('//Output untuk Input ' + loop)
-    } else if (loop == 3) {
-        var nama = 'Danu'
-        var peran = 'tabib'
-        console.log('//Output untuk Input ' + loop)
-    } else if (loop == 4) {
-        var nama = 'Zero'
-        var peran = 'penyihir'
-        console.log('//Output untuk Input ' + loop)
-    }
+/* 
+Menggunakan If Else
+Objectives
 
-    if (nama === '') {
-        console.log('Nama harus diisi!')
-    } else if (peran === '') {
-        console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!')
-    } else if (peran === 'ksatria') {
-        console.log('Selamat datang di dunia Proxytia, ' + nama + '\n ' +
-            'Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!')
-    } else if (peran === 'tabib') {
-        console.log('Selamat datang di dunia Proxytia, ' + nama + '\n' + 'Halo tabib ' + nama + ', kamu akan membantu temanmu yang terluka.')
-    } else if (peran === 'penyihir') {
-        console.log('Selamat datang di dunia Proxytia, ' + nama + '\n' + 'Halo tabib ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!')
+    Mengerti Cara Menggunakan If-Else
+    Mengerti Logika If-Else
+    Mengerti Cara Menggunakan Operator Evaluasi ===, !==
+
+Directions
+
+Kamu diminta untuk memprogram suatu game sederhana, Proxytia namanya. Untuk memulai game itu diperlukan 2 variabel (untuk sekarang), yaitu nama dan peran. Variabel peran harus memiliki isi data, bila kosong pemain akan diberikan peringatan berupa "Pilih Peranmu untuk memulai game". Terdapat 3 peran berbeda yaitu Ksatria, Tabib, dan Penyihir. Tugas Anda adalah untuk membuat program yang mengecek isi variabel peran serta mengeluarkan respon sesuai isi variabel tersebut.
+Hints
+
+    Variabel tetap di-input secara manual.
+    Variabel nama dan peran dapat diisi apa saja.
+    Nama tidak perlu dicek sama persis seperti contoh input/output
+    Buat If-Else berbeda masing-masing untuk mengecek peran
+
+    Input:
+    //Contoh input
+    var nama = 'Mikael';
+    var peran = '';
+
+    Silakan ganti nilai nama dan peran untuk mengetes kondisi!
+Output:
+
+// Output untuk Input nama = '' dan peran = ''
+"Nama harus diisi!"
+
+//Output untuk Input nama = 'Mikael' dan peran = ''
+"Halo Mikael, Pilih peranmu untuk memulai game!"
+
+//Output untuk Input nama = 'Nina' dan peran 'Ksatria'
+"Selamat datang di Dunia Proxytia, Nina"
+"Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!"
+
+//Output untuk Input nama = 'Danu' dan peran 'Tabib'
+"Selamat datang di Dunia Proxytia, Danu"
+"Halo Tabib Danu, kamu akan membantu temanmu yang terluka."
+
+//Output untuk Input nama = 'Zero' dan peran 'Penyihir'
+"Selamat datang di Dunia Proxytia, Zero"
+"Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!"
+
+// var namaD = 'ujang'
+// var namaB = 'arisandi'
+// var nama = namaD + namaB
+// console.log(nama);
+*/
+var nama = 'Michael'
+var peran = 'Penyihir'
+var pesan = ''
+if (nama !== '') {
+    if (peran === '') {
+        console.log('Halo ' + nama + ', pilih peranmu untuk memulai game!');
     } else {
-        console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game')
+        if (peran === 'Kesatria') {
+            pesan = 'Halo Kesatria ' + nama + ', kamu dapat menyerang dengan senjatamu '
+        } else if (peran === 'Tabib') {
+            pesan = 'Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka '
+        } else if (peran === 'Penyihir') {
+            pesan = 'Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu '
+
+        }
+        console.log('Selamat datang di Dunia Proxitya, ' + nama);
+        console.log(pesan);
     }
-    loop++;
+} else {
+    console.log('Nama harus diisi!');
 }
